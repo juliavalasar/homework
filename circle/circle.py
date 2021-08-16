e6507794e8b97f30e92c4e5dbad816ee5496c0a2
juliavalasar <88669593+juliavalasar@users.noreply.github.com>
2021-08-16 15:18:24 +0300

    Update circle.py

--- circle/circle.py
+++ circle/circle.py
@@ -4,13 +4,14 @@
 # А внук Вася любит математику и знает, что периметр и площадь блина можно найти по диаметру сковородки.
 # Напишите программу, которая поможет Васе проверить его вычисления.
 
-
+from math import pi
 def perimeter(diameter):
-    pass
+    return pi * d
 
 
 def square(diameter):
-    pass
+    r = d / 2
+    return pi * r ** 2
 
 
 if __name__ == "__main__":
